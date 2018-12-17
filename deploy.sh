e64d522677097f527bb1cff96ae0ed0da339f4de
wfatec <chao125010@163.com>
2018-12-17 15:49:24 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,12 @@ echo "Update source code..."
 
 git pull
 
+echo "Clear old container..."
+
+docker container rm -f  JavaScriptToES6
+
 echo "Start server..."
 
-docker run -p 4000:4000 -d -v /root/workspace/JavaScriptToES6/:/srv/gitbook fellah/gitbook
+docker run -p 4000:4000 -d --name JavaScriptToES6 -v /root/workspace/JavaScriptToES6/:/srv/gitbook fellah/gitbook
 
 echo "============== End deploy ============="
